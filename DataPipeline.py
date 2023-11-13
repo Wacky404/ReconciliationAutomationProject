cd32a5c4846252d606c4408f3aca2c3d957eeb8b
Wacky404 <77279425+Wacky404@users.noreply.github.com>
2023-11-13 16:09:42 -0600

    Rearranged DataFile.py
    Moved the openai functions out of datafile and into its own class for easier comprehension and reviewability/workability. Fixed master script to run all modules.

--- DataPipeline.py
+++ DataPipeline.py
@@ -1,4 +1,6 @@
+# googleintegration is going to need to be changed same as ReconcileAI file
 from DataFile import DataFile as df
+from ReconcileAI import ReconcileAI as ai
 
 try:
     amount = int(input('How many states are you reconciling/cleansing?(integer): '))
@@ -67,7 +69,7 @@ while go:
             state[i].clean_institution(state[i].wb_uasys, state[i].ws_uasys, state[i].raw_file, df.full_spellings)
             state[i].clean_campuslocation(state[i].wb_uasys, state[i].ws_uasys, state[i].raw_file, df.full_spellings)
 
-            print('Clean is done for ' + str(state[i]) + '\n')
+            print('Clean is done for ' + str(*state[i]) + '\n')
     elif user_choice == 4:
         for i in range(len(state)):
             state[i] = df(file_location[i], worksheet[i], abrev_state[i])
@@ -80,29 +82,31 @@ while go:
                                               df.ws_data_grab, df.ws_nces_grab)
             #state[i].reconcile_google(state[i].wb_uasys, state[i].ws_uasys, df.null_values)
 
-            print('Reconcile is done for ' + str(state[i]) + ' moving on to AI....\n')
+            print('Reconcile is done for ' + str(*state[i]) + ' moving on to AI....\n')
 
-            state[i].ai_institution(state[i].wb_uasys, state[i].ws_uasys, state[i].raw_file)
-            state[i].ai_campuslocation(state[i].wb_uasys, state[i].ws_uasys, state[i].raw_file)
+            state_ai = ai(file_location[i], worksheet[i], abrev_state[i])
+            state_ai.ai_institution(state[i].wb_uasys, state[i].ws_uasys, state[i].raw_file)
+            state_ai.ai_campuslocation(state[i].wb_uasys, state[i].ws_uasys, state[i].raw_file)
 
-            print('AI is done for ' + str(state[i]) + ' moving on to cleaning....\n')
+            print('AI is done for ' + str(*state[i]) + ' moving on to cleaning....\n')
 
             state[i].clean_governing(state[i].wb_uasys, state[i].ws_uasys, state[i].raw_file, df.full_spellings)
             state[i].clean_institution(state[i].wb_uasys, state[i].ws_uasys, state[i].raw_file, df.full_spellings)
             state[i].clean_campuslocation(state[i].wb_uasys, state[i].ws_uasys, state[i].raw_file, df.full_spellings)
 
-            print('Clean is done for ' + str(state[i]) + '\n')
+            print('Clean is done for ' + str(*state[i]) + '\n')
     elif user_choice == 5:
         for i in range(len(state)):
             state[i] = df(file_location[i], worksheet[i], abrev_state[i])
 
-            state[i].ai_institution(state[i].wb_uasys, state[i].ws_uasys, state[i].raw_file)
+            state_ai = ai(file_location[i], worksheet[i], abrev_state[i])
+            state_ai.ai_institution(state[i].wb_uasys, state[i].ws_uasys, state[i].raw_file)
 
-            print('AI is done for ' + str(state[i]) + ' institutions\n')
+            print('AI is done for ' + str(*state[i]) + ' institutions\n')
 
-            state[i].ai_campuslocation(state[i].wb_uasys, state[i].ws_uasys, state[i].raw_file)
+            state_ai.ai_campuslocation(state[i].wb_uasys, state[i].ws_uasys, state[i].raw_file)
 
-            print('AI is done for ' + str(state[i]) + '\n')
+            print('AI is done for ' + str(*state[i]) + '\n')
     else:
         print('You did not input any integer between 1 - 5, please try again\n')
 
--- googleintegration.py
+++ googleintegration.py
@@ -1,7 +1,31 @@
-# import googlemaps
+# Might just be one function, going to placeholder it with a template for now.
 import requests
-import json
 import time
 
-API_KEY = open(r"placeholder").read()
+
 class GoogleIntegration:
+    api_key = open(r"placeholder").read()
+    url = str("https://places.googlemaps.com/v1/places:search_request")
+
+    def __init__(self, query, result):
+        self.query = query
+        self.result = result
+
+    @classmethod
+    def placeholder(cls, url, query, api_key):
+        # Template request to get place_id for location search. Subject to change.
+        r = requests.get(url + 'query=' + query + '&key' + api_key )
+        result = r.json()
+        remove_id = result['results']
+
+        place_id = []
+        for i in range(len(remove_id)):
+            print(remove_id[i]['names'])
+            place_id.append(remove_id)
+        return place_id
+
+    @classmethod
+    def placeholder_1(cls):
+
+    @classmethod
+    def placeholder_2(cls):
